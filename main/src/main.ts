881ade562fe7bc7ec663f59e0ec0726e8fa336ad
xiaoyuzs <3173945434@qq.com>
2024-09-12 17:38:50 +0800

    feat:wujie

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,4 +1,3 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +5,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Wujie from 'wujie-vue3'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.use(Wujie)
+
 app.mount('#app')
+
+
--- web/vue-app/src/App.vue
+++ web/vue-app/src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <h1>这是Vue3子应用</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
